00409f5fc536e705e6d36c0f9c0c2f50aa3f4a5a
Zachary Skalko <zskalko@gmail.com>
2016-02-26 21:20:00 -0600

    Add getNames.php

---
+++ getNames.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+
+$profiles_raw = shell_exec('netsh wlan show profiles');
+
+preg_match_all("/(All|Current) User Profile +: (.+)/", $profiles_raw, $profiles);
+array_shift($profiles);
+
+$_SESSION['profiles'] = array();
+
+$scopes = $profiles[0];
+$ssids  = $profiles[1];
+
+for($i = 0 ; $i < count($scopes); $i++){
+  
+  $_SESSION['profiles'][$i] = array(
+  
+    'SSID' => $ssids[$i],
+    'scope' => $scopes[$i],
+  
+  );
+
+}
+
+echo json_encode($_SESSION['profiles']);
+
+?>
